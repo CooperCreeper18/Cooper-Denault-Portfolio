0e11b28b1192f9f91cf20dea9726dd9733bffd25
CooperCreeper18 <100331876+CooperCreeper18@users.noreply.github.com>
2022-10-27 22:12:01 -0500

    added assignment 6

---
+++ Assignment6/Assets/MyFirstPersonPlayer/Scripts/GameWinText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+/*
+ * Cooper Denault
+ * Assignment 5B
+ * Sets Game Over Screen to true and allows restart
+ */
+
+
+public class GameWinText : MonoBehaviour
+{
+    public GameObject gameWinText;
+    private bool gameOver = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            gameWinText.SetActive(true);
+            gameOver = true;
+        }
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && gameOver == true)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
---
+++ Assignment6/Assets/MyFirstPersonPlayer/Scripts/Target.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Cooper Denault
+ * Assignment 5B
+ * Makes objects have health and can take damage
+ */
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
