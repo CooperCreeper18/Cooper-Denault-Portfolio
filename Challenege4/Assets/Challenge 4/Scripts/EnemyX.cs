8b138a1b50a4f1c8db151234bc1325c7871cd56c
CooperCreeper18 <100331876+CooperCreeper18@users.noreply.github.com>
2022-11-14 00:26:27 -0600

    finished challenge4

---
+++ Challenege4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyX : MonoBehaviour
+{
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+
+    public float speed;
+    public float waveSpeed;
+
+    private SpawnManagerX spawnManagerX;
+    private UIManager uIManagerX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10f;
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
+        spawnManagerX = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<SpawnManagerX>();
+        uIManagerX = GameObject.FindGameObjectWithTag("UIManager").GetComponent<UIManager>();
+        waveSpeed = speed * (spawnManagerX.waveCount - 1);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * waveSpeed * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            uIManagerX.touch = true;
+            Destroy(gameObject);
+
+        }
+
+    }
+
+}
---
+++ Challenege4/Assets/Challenge 4/Scripts/UIManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text waveNumberText;
+    public GameObject winText;
+    public GameObject startText;
+    public GameObject loseText;
+
+    public PlayerControllerX player;
+
+    public SpawnManagerX spawnManager;
+
+    public bool gameOver = false;
+
+    public bool touch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnManager = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<SpawnManagerX>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControllerX>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            startText.SetActive(false);
+        }
+
+        waveNumberText.text = "Wave Number: " + (spawnManager.waveCount - 1 );
+
+        //if statement so when enemy touches goal, you lose
+        if (touch)
+        {
+            gameOver = true;
+            loseText.SetActive(true);
+
+            Destroy(player);
+
+            if (Input.GetKeyDown(KeyCode.R) && gameOver == true)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+        if (spawnManager.waveCount >= 10)
+        {
+            gameOver = true;
+            winText.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.R) && gameOver == true)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+
+        }
+
+    }
+}
