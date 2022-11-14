3b6578412f59371aadc71f20e45080ff32f6ef6f
CooperCreeper18 <100331876+CooperCreeper18@users.noreply.github.com>
2022-11-13 21:51:15 -0600

    finished proto4

---
+++ Prototype4/Assets/Scripts/UIManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text waveNumberText;
+    public GameObject winText;
+    public GameObject loseText;
+    public GameObject startText;
+
+    public PlayerController player;
+
+    public SpawnManager spawnManager;
+
+    private bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnManager = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<SpawnManager>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            startText.SetActive(false);
+        }
+
+        waveNumberText.text = "Wave Number: " + spawnManager.waveNumber;
+
+        if (player.transform.position.y < -10)
+        {
+            gameOver = true;
+            loseText.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.R) && gameOver == true)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+
+        }
+
+        if (spawnManager.waveNumber >= 10)
+        {
+            gameOver = true;
+            loseText.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.R) && gameOver == true)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+
+        }
+
+    }
+}
